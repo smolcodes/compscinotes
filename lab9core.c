46ef1cdbb2eabe2122352edf5609fe604afd613e
Capybar <dubiousradical@hotmail.com>
2018-11-13 13:37:58 -0500

    Add files via upload

---
+++ lab9core.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a=1;
+int b=2;
+int c=a+b;
+int d=a+b+c;
+int e=a+b+c+d;
+printf("%d\n", a);
+printf("%d\n", b);
+printf("%d\n", c);
+printf("%d\n", d);
+printf("%d\n", e);
+return 0;
+}
